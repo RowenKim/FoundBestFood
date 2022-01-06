414bf6822e91eb965cade233c6d55359f19313a2
RowenKim <jw909069@naver.com>
2022-01-06 13:59:52 +0900

    첫 셋팅 작업중

---
+++ src/main/java/com/lec/spring/meal/FoundBestFoodApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring.meal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundBestFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundBestFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lec/spring/meal/FoundBestFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundBestFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
